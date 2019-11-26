fa4ffc376498d787dedc228b8eabc977d71854e3
Nikita <nikitabansal2016@gmail.com>
2019-11-26 14:19:10 +0530

    offers

---
+++ app/src/main/java/in/ac/gla/miniProject/Photographer/Offers.java
@@ -0,0 +1,14 @@
+package in.ac.gla.miniProject.Photographer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Offers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offers);
+    }
+}

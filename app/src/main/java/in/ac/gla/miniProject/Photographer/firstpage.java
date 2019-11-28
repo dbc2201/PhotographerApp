4bdf2b4deaf508213f9efaead35aefc6cb0683b5
Jayendra singh <jayendra.singh_cs17@gla.ac.in>
2019-11-28 21:58:02 +0530

    upload

---
+++ app/src/main/java/in/ac/gla/miniProject/Photographer/firstpage.java
@@ -0,0 +1,49 @@
+package in.ac.gla.miniProject.Photographer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class firstPage extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firstpage);
+        Boolean isFirstTime;
+
+
+        SharedPreferences app_preferences = PreferenceManager
+                .getDefaultSharedPreferences(firstPage.this);
+
+
+        SharedPreferences.Editor editor = app_preferences.edit();
+
+        isFirstTime = app_preferences.getBoolean("isFirstTime", true);
+
+
+        if (isFirstTime) {
+
+            Intent intent = new Intent(firstPage.this,MainActivity.class);
+            startActivity(intent);
+
+//implement your first time logic
+            editor.putBoolean("isFirstTime", false);
+            editor.commit();
+
+        }else{
+
+            Intent intent = new Intent(firstPage.this,Splash.class);
+            startActivity(intent);
+//app open directly
+        }
+    }
+}
+
+
